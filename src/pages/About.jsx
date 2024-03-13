1c79b9a4969a33bb6782a09fe00988e1fe565c0a
lukam <luka.milic77@gmail.com>
2024-03-13 23:14:52 +0100

    final

---
+++ src/pages/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+        asperiores voluptatum, aliquam iure qui harum molestias accusantium id
+        neque sapiente sit! Accusantium beatae error atque mollitia
+        exercitationem laudantium ipsa consequatur.
+      </p>
+    </section>
+  );
+};
+
+export default About;
